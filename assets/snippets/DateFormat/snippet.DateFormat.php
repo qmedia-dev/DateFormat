4e14b8d46f250f10101fa5d48238dd560af34f82
cgehuzi <mail.cgehuzi@gmail.com>
2019-10-08 13:57:54 +0300

    [ fix ] - правит приведение даты к unixtime

--- assets/snippets/DateFormat/snippet.DateFormat.php
+++ assets/snippets/DateFormat/snippet.DateFormat.php
@@ -6,11 +6,15 @@ setlocale(LC_ALL, 'ru_RU.UTF-8');
 // определяем формат
 $format = isset($format) ? $format : '%d.%m.%Y';
 
-// если дата не найдена, выводим текущую
-$date = isset($date) ? $date : date('Y-m-d H:i:s');
+// если дата не найдена, выводим дату публикации
+if (!isset($date)) {
+    $date = ($modx->documentObject['pub_date']) ? $modx->documentObject['pub_date'] : $modx->documentObject['createdon'];
+}
 
 // приводим дату к формату unixtime
-$date = strtotime($date) > 0 ? strtotime($date) : $date;
+if (!is_numeric($date)) {
+    $date = strtotime($date);
+}
 
 // форматируем дату
 $time = strftime($format, $date);
